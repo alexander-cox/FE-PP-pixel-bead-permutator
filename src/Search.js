5c5e59d362bb8dc8f39d446adec9592d5c44da30
Alexander-Cox <alexander.b.cox@outlook.com>
2018-07-21 17:29:39 +0100

    basic setup for search page

--- src/Search.js
+++ src/Search.js
@@ -1,21 +1,50 @@
 import React, { Component } from 'react';
+import Solutions from './components/Solutions';
 import './Search.css';
 
 class Search extends Component {
-    state = { 
+    state = {
+        titleInput: '',
+        tagsInput: '',
         solutions: []
-     }
-    render() { 
-        return ( 
-            <div>
+    }
+    componentDidMount() {
+        fetch('http://localhost:3000/api/solutions')
+            .then((solutions) => solutions.json())
+            .then(solutions => this.setState({ solutions }));
+    }
+    render() {
+        return (
+            <div id='search' className='search background-color-secondary-1-0'>
                 <div className="hero background-color-secondary-1-2">
                     <p>Search for Solutions</p>
                 </div>
+                <SearchForm />
                 <div>
+                    <Solutions solutions={this.state.solutions} />
                 </div>
             </div>
-         );
-    }   
+        );
+    }
 }
- 
+
 export default Search;
+
+const SearchForm = () => {
+    return (
+        <div id="SearchForm" className="">
+            <div className="field ">
+                <label className="label">Search By Title</label>
+                <div className="control">
+                    <input className="input is-small searchinput" type="text" placeholder="Text input" />
+                </div>
+            </div>
+            <div className="field">
+                <label className="label">Search By Tags</label>
+                <div className="control">
+                    <input className="input is-small searchinput" type="text" placeholder="Text input" />
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/Solutions.js
+++ src/components/Solutions.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import Mini_Solution from './Mini_Solution';
 import './Solutions.css';
 
-const Solutions = () => {
+const Solutions = (props) => {
+    //const { solutions } = props;
     return ( 
-        <div>
-
+        <div className="solutions background-color-secondary-1-3">
+            {
+                props.solutions.map(s => <h1 key={s.id}>{s.title}</h1>)
+            }
         </div>
      );
 }
