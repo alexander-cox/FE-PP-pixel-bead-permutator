d6a5923d2d5e9ff5dd25a65ebd5ebd35fec37e7d
Alexander-Cox <alexander.b.cox@outlook.com>
2018-07-22 12:37:47 +0100

    add lower portion of create form to include hashtags and title etc

--- src/Create.js
+++ src/Create.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Pixel_Board from './components/Pixel_Board';
 import './Create.css';
 
 class Create extends Component {
@@ -10,7 +11,7 @@ class Create extends Component {
                     <p>Create some Pixel Art!</p>
                 </div>
                 <div id="CreateForm" >
-                    <div className="field ">
+                    <div className="field">
                         <label className="label">Image URL:</label>
                         <div className="control">
                             <input className="input is-small" type="text" placeholder="Text input" />
@@ -34,8 +35,57 @@ class Create extends Component {
                             </div>
                         </div>
                         <div className='card-content is-flex is-horizontal-center'>
-                            <input className="button button-create" type="submit" value="Create Solution" />
+                            <input className="button button-create" type="submit" value="Generate Solution" />
                         </div>
+                    </div>
+                </div>
+                <Pixel_Board />
+                <div id="CreateForm" >
+                    <div className="field">
+                        <label className="label">Solution Title:</label>
+                        <div className="control">
+                            <input className="input is-small" type="text" placeholder="Text input" />
+                        </div>
+                        <br />
+                        <label className="label">Solution Hashtags:</label>
+                        <div className="columns">
+                            <div className="column">
+                                <div className="control">
+                                    <input className="input is-small" type="text" placeholder="Text input" />
+                                </div>
+                            </div>
+                            <div className="column">
+                                <div className=''>
+                                    <input className="button button-create is-small" type="submit" value="Add Hashtag" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="field is-grouped is-grouped-multiline">
+                            <div class="control">
+                                <div class="tags has-addons">
+                                    <span class="tag">Hashtag1</span>
+                                    <span class="tag is-delete"></span>
+                                </div>
+                            </div>
+
+                            <div class="control">
+                                <div class="tags has-addons">
+                                    <span class="tag">Hashtag2</span>
+                                    <span class="tag is-delete"></span>
+                                </div>
+                            </div>
+
+                            <div class="control">
+                                <div class="tags has-addons">
+                                    <span class="tag">Hashtag3</span>
+                                    <span class="tag is-delete"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='card-content is-flex is-horizontal-center'>
+                            <input className="button button-create" type="submit" value="Submit Solution" />
+                        </div>
+
                     </div>
                 </div>
             </div>
