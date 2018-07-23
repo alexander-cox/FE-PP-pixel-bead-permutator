9ab0e442b0ade7076b8a582004760a5f7423ed7c
Alexander-Cox <alexander.b.cox@outlook.com>
2018-07-23 17:26:12 +0100

    refactor Pictogram from a stateful class to a stateless functional component

--- src/components/Pictogram.js
+++ src/components/Pictogram.js
@@ -2,36 +2,16 @@ import React, { Component } from 'react';
 import ProgressBar from './ProgressBar';
 import './Pictogram.css';
 
-
-class Pictogram extends Component {
-    state = {
-        total: 0,
-        items: []
-    }
-    componentDidMount = () => {
-        const { total, items } = this.props;
-        this.setState({ total, items });
-    }
-    componentDidUpdate(prevProps) {
-        if (this.props.total !== prevProps.total) {
-            const { total, items } = this.props;
-            this.setState({ total, items });
+const Pictogram = ({total, items, resetInventoryState}) => (
+    <div class="pictogram">
+        {
+            items.map((item) => {
+                return (
+                    <ProgressBar key={item.bead_id} bead={item} total={total} resetInventoryState={resetInventoryState}/>
+                )
+            })
         }
-    }
-    render() {
-        const { total, items } = this.state;
-        return (
-            <div class="pictogram">
-                {
-                    items.map((item) => {
-                        return (
-                            <ProgressBar bead={item} total={total} />
-                        )
-                    })
-                }
-            </div>
-        );
-    }
-}
+    </div>
+);
 
 export default Pictogram;
